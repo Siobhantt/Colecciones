eef5ec2ea0d0b53b54c73f167924864323dbc539
Siobhantt <luisa.cardozo@iesnervion.es>
2023-05-04 09:19:06 +0200

    Terminado ej5

--- Colecciones/src/ejercicios/Ej5.java
+++ Colecciones/src/ejercicios/Ej5.java
@@ -4,11 +4,12 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Ej5 {
-
+	static Scanner lee = new Scanner(System.in);
+static HashMap <String, Double> productos = new HashMap<>(); 
 	public static void main(String[] args) {
-		Scanner lee = new Scanner(System.in);
+		
 		//Tengo que acordarme que los tipos que se le pongan a las listas y mapas deben empezar en mayuscula
-		HashMap <String, Double> productos = new HashMap<>(); 
+		
 		String nombre="";
 		double precioP=0;
 		int opc=0;
@@ -18,13 +19,16 @@ public class Ej5 {
 		opc=lee.nextInt();
 		switch(opc) {
 		case 1:
-			
+			añadir(nombre, precioP);
 			break;
 		case 2:
+			quitar(nombre);
 			break;
 		case 3:
+		listado();
 			break;
 		case 0:
+			adios();
 			break;	
 		default:
 			System.out.println("Opcion no contemplada");
@@ -33,13 +37,36 @@ public class Ej5 {
 		}while(opc!=0);
 	}
 public static void menu() {
-	System.out.println("/////////////////////////////////////////////////////////////");
-	System.out.println("||                               PRODUCTOS                            ||");
+	System.out.println("Seleccione la opcion que prefiera.");
+	System.out.println("");
 	
 	System.out.println("1.Alta del producto.");
 	System.out.println("2.Baja del producto.");
 	System.out.println("3.Lista de existencias.");
 	System.out.println("0.Salir.");
 }
+public static void añadir(String nombre, double precio) {
+	System.out.println("Por favor introduzca el nombre del producto al que quiere dar de alta.");
+	nombre = lee.next();
+	System.out.println("Por favor introduzca el precio del producto.");
+	precio=lee.nextDouble();
+	productos.put(nombre, precio);
+	System.out.println();
+}
 
+public static void quitar(String nombre) {
+	System.out.println("Por favor introduzca el nombre del producto que desea retirar.");
+	nombre = lee.next();
+	productos.remove(nombre);
+	System.out.println();
+}
+public static void listado() {
+	System.out.println();
+	System.out.println("Los productos hasta ahora registrados.");
+	System.out.println(productos);
+	System.out.println();
+}
+public static void adios() {
+	System.out.println("Hasta luego!!");
+}
 }
